ab16a40089af2ce0c9230d6c94175d223c859a44
Mayank <poojary.mayank@gmail.com>
2026-01-22 11:44:59 +0530

    cat_me -- server creation

---
+++ 01_Origin/server.js
@@ -0,0 +1,5 @@
+const express = require('express') // calling the express package through the node modules after it being imported through node package manager (npm) 
+
+const server = express() // creating a server instance 
+
+server.listen(3000) // starting the server instance on the port 3000
